692c56e9812bd0d4a58bfc2ea22724210a73ec35
ovalle15 <74880762+ovalle15@users.noreply.github.com>
2020-12-04 16:04:05 -0500

    Homework site
    This is the files for the homework site

---
+++ ajax.js
@@ -0,0 +1,31 @@
+// function dosomething(id) {
+//     id.innerHTML = "fetched....";
+//     }
+
+window.onload = function() {
+(function() {
+    var httpRequest;
+    document.getElementById("button").onclick = function() {
+        makeRequest("data.json");
+    };
+    function makeRequest(url) {
+        httpRequest = new XMLHttpRequest();
+
+        if(!httpRequest){
+            alert("Exiting: Cannot create an XMLHTTP instance");
+            return false;
+        }
+        httpRequest.onreadystatechange = alertContents;
+        httpRequest.open("GET", url);
+        httpRequest.send();
+    }
+    function alertContents() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if(httpRequest.status === 200) {
+                document.write(httpRequest.responseText);
+            } else {
+                alert("There was a problem with the request");
+            }
+        }
+    }})();
+}
