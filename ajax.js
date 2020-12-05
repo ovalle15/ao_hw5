01f2208c7c20cdf8bad4af11e281d0b491d9932c
ovalle15 <74880762+ovalle15@users.noreply.github.com>
2020-12-04 19:09:17 -0500

    json parse

--- ajax.js
+++ ajax.js
@@ -29,11 +29,7 @@ window.onload = function() {
     function alertContents() {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if(httpRequest.status === 200) {
-                var response = httpRequest.responseText
-                $.ajax ({
-                    responseInJson = jQuery.parseJSON(response.responseText)
-                })
-                document.write(responseInJson)
+                    responseInJson = JSON.parse(httpRequest.responseText)
 
 
             } else {
