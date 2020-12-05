1daf93b10c1def3b0e0ff9fd5f5186bed69bcb1e
ovalle15 <74880762+ovalle15@users.noreply.github.com>
2020-12-04 21:24:01 -0500

    more testing

--- ajax.js
+++ ajax.js
@@ -2,45 +2,43 @@
 
 
 
-const generateRows = () => {
+// const generateRows = () => {
 
-}
+// }
 
-const generateHeaders = () => {
-
-
-}
-
-const generateTable = () => {
-
-    let tableHTML = "<table>"
-
-        tableHTML += "<tr>"
-            tableHTML += "<td>&nbsp;</td>"
-            tableHTML += "<th>Program/Major</th>"
-            tableHTML += "<th>Degree Type</th>"
-            tableHTML +="<th>Graduation Year</th>"
-        tableHTML += "</tr>"
-
-        tableHTML += "<tr>"
-        for (var i = 0; i < list1.length; i++){
-            tableHTML += "<th>${list1[i].name}</th>"
-            // var schoolNameValue = ;
-            // var MajorValue = list1[i].major;
-            // var DegreeValue = list1[i].degree;
-            // var graduationDateValue = list1[i].graduation;
-     }
-
-        tableHTML += "</tr>"
+// const generateHeaders = () => {
 
 
+// }
 
-    tableHTML += "</table>"
+// const generateTable = () => {
+
+//     let tableHTML = "<table>"
+
+//         tableHTML += "<tr>"
+//             tableHTML += "<td>&nbsp;</td>"
+//             tableHTML += "<th>Program/Major</th>"
+//             tableHTML += "<th>Degree Type</th>"
+//             tableHTML +="<th>Graduation Year</th>"
+//         tableHTML += "</tr>"
+
+//         tableHTML += "<tr>"
+//         for (var i = 0; i < list1.length; i++){
+//             tableHTML += "<th>${list1[i].name}</th>"
+//             // var schoolNameValue = ;
+//             // var MajorValue = list1[i].major;
+//             // var DegreeValue = list1[i].degree;
+//             // var graduationDateValue = list1[i].graduation;
+//      }
+
+//         tableHTML += "</tr>"
 
 
-}
+
+//     tableHTML += "</table>"
 
 
+// }
 
 window.onload = function() {
 (function() {
@@ -62,7 +60,8 @@ window.onload = function() {
     function alertContents() {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if(httpRequest.status === 200) {
-                document.write(JSON.parse(httpRequest.responseText))
+                jsondoc = httpRequest.responseText
+                document.write(JSON.parse(jsondoc))
             } else {
                 alert("There was a problem with the request");
             }
